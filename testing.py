f49f085e5b3b0707382259fff77fb35b5a058033
DGreegman <obeagug@gmail.com>
2024-07-19 18:20:08 +0100

    Refactored our code in the testing.py

--- testing.py
+++ testing.py
@@ -1,12 +1,13 @@
 def same_data_type(lst):
     if not isinstance(lst, list):
-        return 'empty'
+        return 'This is not a list'
     firt_item = type(lst[0])
-    
+    if len(lst) == 1:
+        return 'You only have one item in the list, Nothing to compare'
     for item in lst: 
         if type(item) != firt_item:
             return False
     
     return True
 
-print(same_data_type(2))  
+print(same_data_type(5))  
